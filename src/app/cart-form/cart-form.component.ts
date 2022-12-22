a92b6e667471580c9de471fde274bbc9844d071e
Renan Costa <renancostam@gmail.com>
2022-12-22 15:33:12 -0300

    Cart form complete

--- src/app/cart-form/cart-form.component.ts
+++ src/app/cart-form/cart-form.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cart-form',
   templateUrl: './cart-form.component.html',
-  styleUrls: ['./cart-form.component.css']
+  styleUrls: ['./cart-form.component.css'],
 })
-export class CartFormComponent {
+export class CartFormComponent implements OnInit {
+  full_name: string = '';
+  address: string = '';
+  credit_card: string = '';
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    // redirect to success page
+    console.log('Submit');
+  }
 }
--- src/app/services/cart/cart.service.ts
+++ src/app/services/cart/cart.service.ts
@@ -1,6 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Product } from 'src/app/models/Product';
 
+const fakeProduct = new Product({
+  id: '1a6e0804-2bd0-4672-b79d-d97027f9071a',
+  name: 'Book',
+  price: 9.99,
+  url: 'https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+  description: 'You can read it!',
+  quantity: 2,
+});
+
 @Injectable({
   providedIn: 'root',
 })
@@ -8,7 +17,7 @@ export class CartService {
   private cart: Product[];
 
   constructor() {
-    this.cart = [];
+    this.cart = [fakeProduct];
   }
 
   public getCart() {
