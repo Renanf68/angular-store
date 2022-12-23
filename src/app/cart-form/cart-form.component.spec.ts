4f191719679587c91f06c874ad4a76ffff9cc41e
Renan Costa <renancostam@gmail.com>
2022-12-23 17:44:17 -0300

    Cart item and cart form tests

--- src/app/cart-form/cart-form.component.spec.ts
+++ src/app/cart-form/cart-form.component.spec.ts
@@ -1,4 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import {
+  ComponentFixture,
+  ComponentFixtureAutoDetect,
+  TestBed,
+} from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { testDeclarations, testImports } from '../app.component.spec';
 
 import { CartFormComponent } from './cart-form.component';
 
@@ -6,18 +13,81 @@ describe('CartFormComponent', () => {
   let component: CartFormComponent;
   let fixture: ComponentFixture<CartFormComponent>;
 
+  let inputName: DebugElement;
+  let inputAddress: DebugElement;
+  let inputCreditCard: DebugElement;
+  let buttonSubmit: DebugElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CartFormComponent ]
-    })
-    .compileComponents();
+      declarations: testDeclarations,
+      imports: testImports,
+      providers: [
+        { provide: ComponentFixtureAutoDetect, useValue: true },
+        // { provide: ContactService, useValue: mockedService }
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CartFormComponent);
     component = fixture.componentInstance;
+
+    inputName = fixture.debugElement.query(By.css('input[name=full_name]'));
+    inputAddress = fixture.debugElement.query(By.css('input[name=address]'));
+    inputCreditCard = fixture.debugElement.query(
+      By.css('input[name=credit_card]')
+    );
+    buttonSubmit = fixture.debugElement.query(By.css('button[type=submit]'));
+
     fixture.detectChanges();
   });
 
+  const fillForm = () => {
+    inputName.nativeElement.value = 'Fire boy';
+    inputName.nativeElement.dispatchEvent(new Event('input'));
+    inputAddress.nativeElement.value = 'Av. beira mar, 48';
+    inputAddress.nativeElement.dispatchEvent(new Event('input'));
+    inputCreditCard.nativeElement.value = '1111111111111111';
+    inputCreditCard.nativeElement.dispatchEvent(new Event('input'));
+  };
+
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(inputName).toBeTruthy();
+    expect(inputAddress).toBeTruthy();
+    expect(inputCreditCard).toBeTruthy();
+    expect(buttonSubmit).toBeTruthy();
+  });
+
+  it('should initiate with empty inputs', () => {
+    expect(inputName.nativeElement.value).toEqual('');
+    expect(inputAddress.nativeElement.value).toEqual('');
+    expect(inputCreditCard.nativeElement.value).toEqual('');
+  });
+
+  it('should initiate with submit button disabled', () => {
+    inputName.nativeElement.value = '';
+    expect(buttonSubmit.properties['disabled']).toBe(true);
+  });
+
+  it('should not be able to submit with invalid data', () => {
+    inputName.nativeElement.value = 'F';
+    inputName.nativeElement.dispatchEvent(new Event('input'));
+    expect(buttonSubmit.properties['disabled']).toBe(true);
+  });
+
+  it('should change values when user types on inputs', () => {
+    fillForm();
+    expect(component.full_name).toEqual('Fire boy');
+    expect(component.address).toEqual('Av. beira mar, 48');
+    expect(component.credit_card).toEqual('1111111111111111');
+  });
+  it('should be able to submit valid data', () => {
+    spyOn(component.onSubmitOrder, 'emit');
+    fillForm();
+    expect(buttonSubmit.properties['disabled']).toBe(false);
+    const form = fixture.debugElement.query(By.css('form'));
+    form.triggerEventHandler('submit', {});
+    fixture.detectChanges();
+    expect(component.onSubmitOrder.emit).toHaveBeenCalled();
   });
 });
--- src/app/cart-item/cart-item.component.spec.ts
+++ src/app/cart-item/cart-item.component.spec.ts
@@ -1,23 +1,63 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { testDeclarations, testImports } from '../app.component.spec';
+import { Product } from '../models/Product';
+import { CartService } from '../services/cart/cart.service';
 
 import { CartItemComponent } from './cart-item.component';
 
 describe('CartItemComponent', () => {
+  let cartService: CartService;
   let component: CartItemComponent;
   let fixture: ComponentFixture<CartItemComponent>;
+  let testProduct: Product;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CartItemComponent ]
-    })
-    .compileComponents();
+      declarations: testDeclarations,
+      imports: testImports,
+    }).compileComponents();
 
+    cartService = TestBed.inject(CartService);
     fixture = TestBed.createComponent(CartItemComponent);
     component = fixture.componentInstance;
+    testProduct = new Product({
+      name: 'Test',
+      description: 'Test product',
+      price: 10.0,
+      url: '',
+      quantity: 2,
+    });
+    cartService.addToCart(testProduct);
+    const cart = cartService.getCart();
+    component.product = cart[0];
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render product infos', () => {
+    expect(component.product.name).toEqual('Test');
+    expect(component.product.price).toEqual(10.0);
+    expect(component.quantity).toEqual(2);
+  });
+
+  it('should be able to call update product output with correct data', async () => {
+    spyOn(component.updateCartProduct, 'emit');
+    component.quantity = 4;
+    component.updateProduct();
+    expect(component.updateCartProduct.emit).toHaveBeenCalledWith({
+      productId: component.product.id,
+      quantity: 4,
+    });
+  });
+
+  it('should be able to call remove product output with correct data', async () => {
+    spyOn(component.removeCartProduct, 'emit');
+    component.removeProduct();
+    expect(component.removeCartProduct.emit).toHaveBeenCalledWith(
+      component.product
+    );
   });
 });
